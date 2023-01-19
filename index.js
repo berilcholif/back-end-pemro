ea7fcf3f61fd6f99f93da7dcdc09cb7d1ba3f919
beril cholif <berilcholif2@gmail.com>
2023-01-19 14:47:21 +0700

    beril

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const port = 3000;
 
+// const http = require('http');
+// var randomWords = require('random-words');
+
+// console.log(randomWords());
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer(function(req, res) {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end(randomWords()+'\n');
+// });
+
+// server.listen(port, hostname, function() {
+//   console.log('Server running at http://'+ hostname + ':' + port + '/');
+// });
+var randomWords = require('random-words');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 4000;
 app.use(express.json());
 app.use(cors());
+
 
 app.get("/getlist", function (req, res) {
   res.send({
@@ -16,8 +34,9 @@ app.get("/getlist", function (req, res) {
     ],
   });
 });
-
-app.listen(port);
+app.listen(port, () => {
+  console.log(`cli-nodejs-api listening at http://localhost:${port}`)
+});
 // const http = require('http');
 // var randomWords = require('random-words');
 
